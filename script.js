aa6cf85aa49342c9d9c91f62d3c34308c04e5948
sofia-grunditz <tora.s.grunditz@gmail.com>
2025-03-05 15:13:38 +0100

    fixed requested changes: hamburger menu and hero image

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-// Hämta referenser till de element vi behöver
-const menuButton = document.getElementById('menu-button');  // Menyknapp
-const navbardialog = document.querySelector("#navbardialog");  // Dialogruta
+const menuButton = document.getElementById("menu-button");
+const navbar = document.getElementById("navbar");
 
-// Lägg till eventlyssnare för att öppna dialogrutan när menyknapp klickas
-menuButton.addEventListener('click', function () {
-  navbardialog.showModal();  // Öppnar dialogrutan
-});
-
-// Lägg till eventlyssnare för att stänga dialogrutan när användaren klickar på bakgrunden
-navbardialog.addEventListener('click', function (event) {
-  if (event.target === this) {
-    navbardialog.close();  // Stänger dialogrutan
-  }
+menuButton.addEventListener("click", () => {
+  navbar.classList.toggle("show");
 });
